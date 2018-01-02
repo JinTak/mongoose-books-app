69c3dcb2f18797c35c095e83ff4c9baaaaff6698
JinTak <jtak23@gmail.com>
2018-01-02 16:33:16 -0700

    working on updating on Sprint 1

--- server.js
+++ server.js
@@ -8,7 +8,8 @@
 
 //require express in our app
 var express = require('express'),
-  bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    db = require('./models');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -18,49 +19,11 @@ app.use(express.static('public'));
 
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-////////////////////
-//  DATA
-///////////////////
-
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-
-
-
-
+app.use(bodyParser.json());
 
 ////////////////////
 //  ROUTES
 ///////////////////
-
-
-
 
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
@@ -70,47 +33,74 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+    });
 });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
-});
+    db.Book.findById(req.params.id, function (err, Book) { res.json(Book) } );
+  });
+
 
 // create new book
 app.post('/api/books', function (req, res) {
-  // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  books.push(newBook);
-  res.json(newBook);
+  // create new book with form data (`req.body`)\
+  var book = new db.Book({
+    title: req.body.title,
+    author: req.body.author,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate
+  })
+
+  book.save(function (err, post) {
+    if (err) { console.log("ERROR"); }
+  });
+
 });
 
 // update book
-// app.put('/api/books/:id', controllers.books.update);
+app.put('/api/books/:id', function (req, res) {
+  // create new book with form data (`req.body`)
+  db.Book.findById(req.params.id, function (err, bookToUpdate) { 
+
+    var updatedBook = new db.Book({
+      title: req.body.title,
+      author: req.body.author,
+      image: req.body.image,
+      releaseDate: req.body.releaseDate
+    });
+    
+    bookToUpdate.save(function (err, updatedBook) {
+      if (err) return handleError(err);
+      res.send(bookToUpdate);
+    });
+
+  });
+
+});
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
-  // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var deleteBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
+
+  db.Book.findById(req.params.id, function (err, Book) {
+    var bookToDelete = Book;
+
+    db.Book.remove( {_id: req.params.id }, (err) => {
+      if(err) {
+        res.json("Error: Object was not removed.")
+        console.log("Error: Object was not removed.")
+      }
+      else {
+        res.json("Successfully removed " + bookToDelete + "!")
+        console.log("Successfully removed " + bookToDelete + "!");}
+    }); 
+    
   });
-  console.log('deleting book with index', deleteBookIndex);
-  var bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
+  
 });
 
 
